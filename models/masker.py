829f0d4e8a42b794426a2d67422ed9f37cf45085
AMSUFV <angelmelchorsanchez@gmail.com>
2020-03-06 14:23:18 +0100

    masker and maskreconstructor

--- models/masker.py
+++ models/masker.py
@@ -28,7 +28,54 @@ class Masker(pix.Pix2Pix):
                 tf.summary.image('prediction', stack, step=epoch, max_outputs=2)
 
 
-def mask_train():
+class MaskReconstructor(pix.Pix2Pix):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.masker = tf.keras.models.load_model('../trained_models/masker_0259_customsave.h5')
+
+    # def _loss_g(self, d_g_x, **kwargs):
+    #     loss_d_g_x = self.loss_object(tf.ones_like(d_g_x), d_g_x)
+    #     # difference between the number of zeros (or mask size) between y and g_x
+    #     loss_diff = kwargs['multiplier'] * tf.abs(1 - tf.reduce_sum(kwargs['y']) / tf.reduce_sum(kwargs['g_x']))
+    #     loss_total = loss_d_g_x + loss_diff
+    #     return loss_total, loss_diff
+    # todo: implementar loss de de area
+    
+    def fit(self, train, validation, epochs=1, log_dir=None):
+        writer_train, writer_val = None, None
+        if log_dir is not None:
+            writer_train, writer_val = self._get_writers(log_dir)
+
+        for epoch in range(epochs):
+            for dataset, writer, training in zip([train, validation], [writer_train, writer_val], [True, False]):
+                for x, y in dataset:
+                    m_x = self.masker(x, training=False)
+                    m_y = self.masker(y, training=False)
+                    missing = tf.abs(m_y - m_x)
+                    x_missing = tf.where(missing == 0.0, x, -1.0)
+
+                    self._step(x_missing, y, epoch, training, writer)
+
+                self._log_images(dataset, writer, epoch)
+
+    def _log_images(self, dataset, writer, epoch):
+        if writer is not None:
+            x, y = next(dataset.take(1).__iter__())
+            m_x = self.masker(x, training=False)
+            m_y = self.masker(y, training=False)
+            missing = tf.abs(m_y - m_x)
+            x_missing = tf.where(missing == 0, x, -1.0)
+
+            g_x = self.generator(x_missing, training=False)
+
+            stack = tf.stack([x, x_missing, y, g_x], axis=0) * 0.5 + 0.5
+            stack = tf.squeeze(stack)
+
+            with writer.as_default():
+                tf.summary.image('prediction', stack, step=epoch, max_outputs=4)
+
+
+def train_mask():
     dataset_tool.setup_paths('../dataset')
     temples = ['temple_0', 'temple_2', 'temple_5', 'temple_9']
     train_ds, val_ds = dataset_tool.get_dataset_segmentation(temples, split=0.3, repeat=2, mask=True)
@@ -36,9 +83,20 @@ def mask_train():
     masker = Masker()
 
     masker.fit(train_ds, val_ds, epochs=5, log_dir='../logs/masking_0259')
-    masker.generator.save('./trained_models/masker_0259.h5')
-    masker.save('./trained_models/masker_0259_customsave.h5')
+    masker.generator.save('../trained_models/masker_0259.h5')
+    masker.save('../trained_models/masker_0259_customsave.h5')
+
+
+def train_mask_reconstruction():
+    dataset_tool.setup_paths('../dataset')
+    temples = ['temple_0', 'temple_2', 'temple_5', 'temple_9']
+    train_ds, val_ds = dataset_tool.get_dataset_reconstruction(['temple_0'], split=0.3, repeat=2)
+
+    mask_reconstructor = MaskReconstructor()
+
+    mask_reconstructor.fit(train_ds, val_ds, epochs=50, log_dir='../logs/missingarea_0')
+    mask_reconstructor.generator.save('../trained_models/missingarea_0.h5')
 
 
 if __name__ == '__main__':
-    mask_train()
+    train_mask_reconstruction()
--- models/trimasker.py
+++ models/trimasker.py
@@ -1,32 +1,31 @@
 import tensorflow as tf
-from models.pix2pix_updated import Pix2Pix
+from models import pix2pix_updated as pix
 from utils import dataset_tool
-import matplotlib.pyplot as plt
 
 
-class MaskReconstructor(Pix2Pix):
-    def __init__(self):
-        super().__init__()
+class MaskReconstructor(pix.Pix2Pix):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.masker = tf.keras.models.load_model('../trained_models/masker_019.h5')
 
     @tf.function
     def _step(self, train_x, train_y, training=True):
         with tf.GradientTape(persistent=True) as tape:
-            g_m_x = self.generator(train_x, training=training)
+            m_x = self.masker(train_x, training=False)
+            m_y = self.masker(train_y, training=False)
+            y = tf.abs(m_y - m_x)
 
-            d_m_y = self.discriminator([train_x, train_y], training=training)
-            d_g_m_x = self.discriminator([train_x, g_m_x], training=training)
+            g_m_x = self.generator(m_x, training=training)
+
+            d_m_y = self.discriminator([m_x, y], training=training)
+            d_g_m_x = self.discriminator([m_x, g_m_x], training=training)
 
             loss_d = self._loss_d(d_m_y, d_g_m_x)
-            loss_g, l1_loss = self._loss_g(d_g_x=d_g_m_x, g_x=g_m_x, y=train_y, multiplier=100)
+            loss_g, l1_loss = self._loss_g(d_g_x=d_g_m_x, g_x=g_m_x, y=y, multiplier=100)
 
         if training:
-            # self._gradient_update(tape, loss_g, self.generator, self.optimizer_g)
-            # self._gradient_update(tape, loss_d, self.discriminator, self.optimizer_d)
-            gradients_g = tape.gradient(loss_g, self.generator.trainable_variables)
-            gradients_d = tape.gradient(loss_d, self.discriminator.trainable_variables)
-            self.optimizer_g.apply_gradients(zip(gradients_g, self.generator.trainable_variables))
-            self.optimizer_d.apply_gradients(zip(gradients_d, self.discriminator.trainable_variables))
+            self._gradient_update(tape, loss_g, self.generator, self.optimizer_g)
+            self._gradient_update(tape, loss_d, self.discriminator, self.optimizer_d)
 
         metrics_names = ['loss_gen_total', 'loss_gen_l1', 'loss_disc']
         metrics = [loss_g, l1_loss, loss_d]
@@ -40,26 +39,25 @@ class MaskReconstructor(Pix2Pix):
 
         for epoch in range(epochs):
             for train_x, train_y in train:
-                m_x = self.masker(train_x, training=False)
-                m_y = self.masker(train_y, training=False)
-                self._step(m_x, m_y)
+                metrics_names, metrics = self._step(train_x, train_y)
+            # self._write_metrics(writer_train, metrics_names, metrics, epoch)
             self._log_images(train, writer_train, epoch)
 
             for val_x, val_y in validation:
-                m_x = self.masker(val_x, training=False)
-                m_y = self.masker(val_y, training=False)
-                self._step(m_x, m_y, training=False)
+                metrics_names, metrics = self._step(val_x, val_y, training=False)
+            # self._write_metrics(writer_val, metrics_names, metrics, epoch)
             self._log_images(validation, writer_val, epoch)
 
     def _log_images(self, dataset, writer, epoch):
         if writer is not None:
             x, y = next(dataset.take(1).__iter__())
-
             m_x = self.masker(x, training=False)
             m_y = self.masker(y, training=False)
+            tar = m_y - m_x
+
             g_m_x = self.generator(m_x, training=False)
 
-            tb_images = [m_x, g_m_x]
+            tb_images = [x, m_x, tar, g_m_x]
             stack = tf.stack(tb_images, axis=0) * 0.5 + 0.5
             stack = tf.squeeze(stack)
 
@@ -67,20 +65,12 @@ class MaskReconstructor(Pix2Pix):
                 tf.summary.image('prediction', stack, step=epoch, max_outputs=len(tb_images))
 
 
-def predictions():
-    model = tf.keras.models.load_model('../trained_models/masker_019.h5')
-    dataset_tool.setup_paths('../dataset')
-    pred_tr, pred_val = dataset_tool.get_dataset_reconstruction(['temple_0'], repeat=2)
-    writer = tf.summary.create_file_writer('../logs/test/masker_test')
-    for i, (image, _) in enumerate(pred_tr):
-        prediction = model(image, training=False) * 0.5 + 0.5
-        # prediction = tf.squeeze(prediction)
-        with writer.as_default():
-            tf.summary.image('prediction', prediction, i)
-
-
 if __name__ == '__main__':
     dataset_tool.setup_paths('../dataset')
-    train_ds, val_ds = dataset_tool.get_dataset_reconstruction(['temple_0'], repeat=2)
-    pix2pix = Pix2Pix()
-    pix2pix.fit(train_ds, val_ds, epochs=10, log_dir='../logs/test')
+    temples = ['temple_0', 'temple_1', 'temple_9']
+    train_ds, val_ds = dataset_tool.get_dataset_reconstruction(temples, repeat=2)
+
+    gen = pix.generator(activation='sigmoid')
+    pix2pix = MaskReconstructor(gen=gen)
+    pix2pix.fit(train_ds, val_ds, epochs=50, log_dir='../logs/test')
+    # predictions()
